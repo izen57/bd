e124b991718ea9e2dec8354b83ea08669b126767
izen57 <golovoslon@gmail.com>
2021-10-20 18:09:27 +0300

    encoding

--- lab02/lab02.sql
+++ lab02/lab02.sql
@@ -1,20 +1,20 @@
--- 1. »нструкци¤ SELECT, использующа¤ предикат сравнени¤.
+-- 1. Инструкция SELECT, использующая предикат сравнения.
 select distinct t.root_number, t.transport_type
 from transport t join transport as t2 on t.transport_type = t2.transport_type
 where t2.start_id != t.start_id and t.entry_date = '1982-08-24'
 order by t.root_number, t.transport_type;
 
--- 2. »нструкци¤ SELECT, использующа¤ предикат BETWEEN.
+-- 2. Инструкция SELECT, использующая предикат BETWEEN.
 select distinct name, address
 from transport_stop
 where install_year between '1997-01-01' and '1997-03-31';
 
--- 3. »нструкци¤ SELECT, использующа¤ предикат LIKE.
+-- 3. Инструкция SELECT, использующая предикат LIKE.
 select distinct root_number, start_id, stop_id
 from fare
 where day_time like 'с 9%';
 
--- 4. »нструкци¤ SELECT, использующа¤ предикат IN с вложенным подзапросом.
+-- 4. Инструкция SELECT, использующая предикат IN с вложенным подзапросом.
 select root_number, start_id, stop_id
 from fare
 where root_number in
@@ -24,7 +24,7 @@ where root_number in
 	where transport_type = 'автобус'
 ) and price > 50;
 
--- 5. »нструкци¤ SELECT, использующа¤ предикат EXISTS с вложенным подзапросом.
+-- 5. Инструкция SELECT, использующая предикат EXISTS с вложенным подзапросом.
 select root, timing
 from timetable
 where exists
@@ -34,7 +34,7 @@ where exists
 	where install_year > '1990-02-23'
 );
 
--- 6. »нструкци¤ SELECT, использующа¤ предикат сравнени¤ с квантором
+-- 6. Инструкция SELECT, использующая предикат сравнения с квантором
 select root, max_price
 from timetable
 where max_price > all
@@ -44,19 +44,19 @@ where max_price > all
 	where start_id = 123
 );
 
--- 7. »нструкци¤ SELECT, использующа¤ агрегатные функции в выражени¤х столбцов.
+-- 7. Инструкция SELECT, использующая агрегатные функции в выражениях столбцов.
 select avg(price)
 from fare
 group by start_id;
 
--- 8. »нструкци¤  SELECT, использующа¤ скал¤рные подзапросы в выражени¤х столбцов.
+-- 8. Инструкция  SELECT, использующая скалярные подзапросы в выражениях столбцов.
 select
 (
 	select name
 	from transport_stop limit 1
 );
 
--- 10. »нструкци¤ SELECT, использующа¤ поисковое выражение CASE.
+-- 10. Инструкция SELECT, использующая поисковое выражение CASE.
 select root,
 case
 	when weekends = true then
@@ -66,10 +66,10 @@ case
 end weekends
 from timetable;
 
--- 16. ќднострочна¤ инструкци¤ INSERT, выполн¤юща¤ вставку в таблицу одной строки значений.
+-- 16. Однострочная инструкция INSERT, выполняющая вставку в таблицу одной строки значений.
 insert into transport values (1, 78, 53, 'троллейбус', '1882-08-24');
 
--- 17. ћногострочна¤ инструкци¤ INSERT, выполн¤юща¤ вставку в таблицу результирующего набора данных вложенного подзапроса.
+-- 17. Многострочная инструкция INSERT, выполняющая вставку в таблицу результирующего набора данных вложенного подзапроса.
 insert into transport
 select
 (
@@ -79,12 +79,12 @@ select
 ), start_id, stop_id, 'маршрутка', '2001-07-06'
 from transport;
 
--- 18. ѕроста¤ инструкци¤ UPDATE.
+-- 18. Простая инструкция UPDATE.
 update timetable
 set timing = timing + '00:15:00'
 where transport_stop_id = 956;
 
--- 19. »нструкци¤ UPDATE со скал¤рным подзапросом в предложении SET.
+-- 19. Инструкция UPDATE со скалярным подзапросом в предложении SET.
 update timetable
 set max_price =
 (
@@ -94,11 +94,11 @@ set max_price =
 )
 where root = 12;
 
--- 20. ѕроста¤ инструкци¤ DELETE.
+-- 20. Простая инструкция DELETE.
 delete from transport_stop
 where install_year = '1999-02-19';
 
--- 23. »нструкци¤  SELECT, использующа¤ рекурсивное обобщенное табличное выражение. 
+-- 23. Инструкция  SELECT, использующая рекурсивное обобщенное табличное выражение. 
 with recursive cte as
 (
 	select '09:00:00' as timing, transport_stop_id, root, weekends, max_price
@@ -113,7 +113,7 @@ select *
 from cte
 order by timing;
 
--- 25. ќконные фнкции дл¤ устранени¤ дублей 
+-- 25. Оконные функции для устранения дублей 
 select t.root, t.transport_stop_id
 into temp temp_table
 from timetable as t join fare as f on t.root = f.root_number;
@@ -129,6 +129,6 @@ where root in
 	where tt.unique_val > 1
 );
 
--- «ащита: вывести кол-во остановок со всеми видами транспорта
+-- Защита: вывести кол-во остановок со всеми видами транспорта
 select count(*)
 from transport as t join transport_stop as ts on (ts.id = t.start_id or ts.id = t.stop_id) and ts.electricity is true and ts.rails is true;
